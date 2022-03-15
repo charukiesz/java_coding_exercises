2be9bc87872e4948736a81a38e934e30706dcc07
Charulata Kieszkiewicz <charulatapatel2002@yahoo.co.uk>
2022-03-15 22:08:17 +0000

    Implement exercise 001 - checkHello, checkInitials

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -5,21 +5,35 @@ import java.util.List;
 public class Exercise001 {
     public String capitalizeWord(String word) {
         // Add your code here
-        return "";
+        return Character.toUpperCase(word.charAt(0)) + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
         // Add your code here
-        return "";
+        String stringFirst = firstName;
+        String stringLast = lastName;
+        String stringInitials = stringFirst.charAt(0)  + "." + stringLast.charAt(0);
+
+        return stringInitials;
     }
 
     public double addVat(double originalPrice, double vatRate) {
         // Add your code here
-        return 0.0;
-    }
+        Double price = originalPrice;
+        Double vat = (price * (vatRate/100));
+        Double priceWithVat =  0.0; // (originalPrice + vat);
+
+        If (vatRate == 0.0){
+            priceWithVat = originalPrice;
+        }else{
+            priceWithVat = (originalPrice + vat);
+        }
+        return priceWithVat;
+
 
     public String reverse(String sentence) {
         // Add your code here
+            
         return "";
     }
 
